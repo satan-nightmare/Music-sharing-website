b6f5d02589295027c32db579ef639f4791e4e711
Sumit Singh <sumitsinghsakhwar@gmail.com>
2017-02-11 23:54:47 +0530

    Add files via upload

---
+++ music/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+app_name='music'
+
+urlpatterns = [
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    # /music/71/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail')
+]
